61367fec8c21119eaf1db6cb5391a398dc7726e7
Alaric von Teplitz <alaric2000@gmail.com>
2026-02-04 17:42:17 -0800

    Clean up the code, get rid of the save button, add auto-reloads on changes, rearrange options

---
+++ defaults.js
@@ -0,0 +1,17 @@
+'use strict';
+
+self.JERKMUTER_DEFAULT_OPTIONS = {
+  st_quotes: true,
+  st_quotepeek: false,
+  st_threads: true,
+  st_phrases: true,
+  st_disable: true,
+  st_name: false,
+  st_peek: false,
+  st_borderColor: '#656565',
+  st_backColor: '#656565',
+  st_textColor: '#cccccc',
+  st_noteText: 'Muted',
+  st_jerks: ['Rude Jerk'],
+  st_keywords: ['literally hitler']
+};
